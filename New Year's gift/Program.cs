bf3429a12212f469fe22068a527b976215541c8d
girl-physicist <girl-physicist@mail.ru>
2017-04-17 12:23:55 +0300

    Добавьте файлы проекта.

---
+++ New Year's gift/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Year_s_gift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
